6b1c3b2e461f5dc743ae6413231fce2abc090402
GeraldSalazar <geraldsalazar4@gmail.com>
2023-04-08 00:25:15 -0600

    code refactor

--- Sources/Inicializador.c
+++ Sources/Inicializador.c
@@ -6,12 +6,15 @@
 #include <sys/shm.h>
 #include <errno.h>
 #include <semaphore.h>
-#include <fcntl.h>            // Necesario para O_CREAT y O_EXCL
+#include <fcntl.h> // Necesario para O_CREAT y O_EXCL
 #include <time.h>
 #include "datosCompartidos.h" // Estructura
 #include "Constantes.h"
 
-int main(int argc, char *argv[]) {
+void printDatos(struct datosCompartida* d);
+
+int main(int argc, char *argv[])
+{
 
     // Valores ingresados
     char *ID;
@@ -19,7 +22,8 @@ int main(int argc, char *argv[]) {
     int numeroEspacio;
 
     // Verificar que se hayan ingresado los 3 argumentos
-    if (argc != 4) {
+    if (argc != 4)
+    {
         printf("Uso: programa <string1> <numero1> <numero2>\n");
         return 1;
     }
@@ -35,69 +39,93 @@ int main(int argc, char *argv[]) {
 
     /* Inicializar semáforos */
     sem_t *sem_llenos, *sem_vacios;
-    
-    // Crear sem_llenos con nombre "sem_llenos" y un valor inicial de 0
+
+    // Semáforo que contiene el num de espacios ocupados del buffer
+    // Con O_EXCL se asegura que si el semáforo ya existe, entonces la función retorna error
     sem_llenos = sem_open("/sem_llenos", O_CREAT | O_EXCL, 0644, 0);
-    if (sem_llenos == SEM_FAILED) {
-        perror("sem_open");
+    if (sem_llenos == SEM_FAILED)
+    {
+        perror("Error abriendo el semáforo de espacios ocupados");
         exit(1);
     }
+    int value;
+    sem_getvalue(sem_llenos, &value);
+    printf("sem_llenos value: %d\n", value);
 
-    // Crear sem_vacios con nombre "sem_vacios" y un valor inicial de numeroEspacio
+    // Semáforo que contiene el num de espacios vacíos del buffer
+    // 0644 son los permisos. 6->owner R/W  4->the group read-only 4->all others read-only
     sem_vacios = sem_open("/sem_vacios", O_CREAT | O_EXCL, 0644, numeroEspacio);
-    if (sem_vacios == SEM_FAILED) {
-        perror("sem_open");
+    if (sem_vacios == SEM_FAILED)
+    {
+        perror("Error abriendo el semáforo de espacios vacíos");
         exit(1);
     }
+    sem_getvalue(sem_vacios, &value);
+    printf("sem_vacios value: %d\n", value);
 
-    // Inicializamos esta memoria compartida
+    // Inicializamos la estructura compartida
     struct datosCompartida *datos;
-    
+
     // Crear una clave única para la memoria compartida
     key_t key = ftok(KEY_PATH, *ID);
     printf("key: %-20d\n", key);
 
-    size_t tamaño = sizeof(struct datosCompartida);
+    size_t tamano = sizeof(struct datosCompartida);
+    printf("tamaño mem compartida: %zu bytes\n", tamano);
 
     // Crear la memoria compartida
-    int shmid = shmget(key, tamaño, 0666 | IPC_CREAT);
-    if (shmid == -1) {
-        perror("shmget");
-        exit(1);
-    }   
-    // Asignar la estructura a la memoria compartida
-    datos = shmat(shmid, NULL, 0);
-    if (datos == (void *) -1) {
-        perror("shmat");
+    int shmid = shmget(key, tamano, 0666 | IPC_CREAT);
+    if (shmid == -1)
+    {
+        perror("Error creando la memoria compartida");
         exit(1);
     }
-
+    printf("ID mem compartida: %d\n", shmid);
+    // Asignar la estructura a la memoria compartida. datos tiene la dirección de la memoria compartida
+    // shmaddr = NULL -> El SO decide la dirección donde estará el segmento de mem compartida
+    datos = shmat(shmid, NULL, 0);
+    if (datos == (void *)-1)          //(void *) -1 es un puntero a la dirección de memoria 0xFFFFFFFF
+    {
+        perror("Error asignando la memoria compartida");
+        exit(1);
+    }
+    printf("Dirección mem compartida: %p\n", datos);
+    
     // Asignar valores a la estructura
-    //datos->buffer = malloc(numeroEspacio * sizeof(int));
     datos->clave = clave;
     datos->numeroEspacio = numeroEspacio;
-    datos->contEmisoresTotal=0;
-    datos->contReceptoresTotal=0;
-    datos->contEmisoresVivos=0;
-    datos->contReceptoresVivos=0;
-    datos->indiceEmisor=0;
-    datos->indiceReceptor=0;
-    datos->indiceTxtEmisor=0;
-    datos->indiceTxtReceptor=0;
+    datos->contEmisoresTotal = 0;
+    datos->contReceptoresTotal = 0;
+    datos->contEmisoresVivos = 0;
+    datos->contReceptoresVivos = 0;
+    datos->indiceEmisor = 0;
+    datos->indiceReceptor = 0;
+    datos->indiceTxtEmisor = 0;
+    datos->indiceTxtReceptor = 0;
 
-    printf("\n");
-    printf("ID: %-20s\n", ID);
-    printf("Clave:%-20d\n",datos->clave);
-    printf("Numero de espacio:%-20d\n",datos->numeroEspacio );
-    
-    datos->buffer[0]=0;
-    datos->buffer[1]=1;
-    datos->buffer[2]=2;
-    datos->buffer[3]=3;
-    datos->buffer[4]=5;
+    // datos->buffer = malloc(numeroEspacio * sizeof(int));
+    datos->buffer[0] = 'P';
+    datos->buffer[1] = 'I';
+    datos->buffer[2] = 'E';
+    datos->buffer[3] = 'S';
+    datos->buffer[4] = ' ';
+    datos->buffer[5] = 'P';
+    datos->buffer[6] = 'A';
+    datos->buffer[7] = 'T';
+    datos->buffer[8] = 'A';
+    datos->buffer[9] = 'S';
 
-    printf("%-20d",datos->buffer[4]);
-    printf("\n");
-
+    printDatos(datos);
     return 0;
+}
+
+void printDatos(struct datosCompartida* d) {
+    printf("Clave: %d\n", d->clave);
+    printf("Espacios del buffer: %d\n", d->numeroEspacio);
+    printf("contEmisoresTotal: %d\n", d->contEmisoresTotal);
+    printf("contReceptoresTotal: %d\n", d->contReceptoresTotal);
+    printf("contEmisoresVivos: %d\n", d->contEmisoresVivos);
+    printf("contReceptoresVivos: %d\n", d->contReceptoresVivos);
+    printf("buffer: %s\n", d->buffer);
+    return;
 }
--- Sources/datosCompartidos.h
+++ Sources/datosCompartidos.h
@@ -2,14 +2,17 @@
 #define datosCompartidos
 
 //Struct que vamos a emplear para guardar en la memoria compartida
+// tamano int->4 bytes, char->1 byte, buffer->256*4bytes
 
+// with int buffer      :  4 + 16 + 8 + 8 + 1 + 100  = 20 + 16 + 101 = 137 + padding bytes from compiler 
+// with char buffer   :  4 + 16 + 8 + 8 + 1 + 25   = 20 + 16 + 26  = 62 + padding bytes from compiler 
 struct datosCompartida {
     int numeroEspacio;
     int indiceEmisor, indiceReceptor, indiceTxtEmisor, indiceTxtReceptor;
     int contEmisoresVivos, contReceptoresVivos;
     int contEmisoresTotal, contReceptoresTotal;
     char clave;
-    int buffer[256];
+    char buffer[25];
     //char buffer[256];
     //FILE * fileTxt;
     //char fileTxtReceptor;
